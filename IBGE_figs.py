d094a0c79200e3e71386a07cc891e22b3972a314
LGDS <gui.lgds2012@outlook.com>
2024-01-15 20:13:31 -0300

    update IBGE figs from Macae Project

--- IBGE_figs.py
+++ IBGE_figs.py
@@ -22,6 +22,12 @@ macae_cor_fig = px.pie(
     title="Distribuição de Cor de Pele Macaé"
     )
 
+macae_cor_fig.update_traces(
+    hovertemplate="<br>".join([
+        "Cor: %{label}",
+        "População: %{value}"
+    ])
+)
 #IBGE dados cor - Brasil
 data = requests.get("https://servicodados.ibge.gov.br/api/v3/agregados/9606/periodos/2022/variaveis/93?localidades=N1[all]&classificacao=86[2776,2777,2778,2779,2780]")
 data_json = data.json()
@@ -41,7 +47,12 @@ brasil_cor_fig = px.pie(
     title="Distribuição de Cor de Pele Brasil"
     )
 
-
+brasil_cor_fig.update_traces(
+    hovertemplate="<br>".join([
+        "Cor: %{label}",
+        "População: %{value}"
+    ])
+)
     
 #IBGE dados crescimento população
 data = requests.get("https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/2001|2002|2003|2004|2005|2006|2008|2009|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020|2021/variaveis/9324?localidades=N6[3302403]")
@@ -56,6 +67,13 @@ for pop in list(data_json[0]["resultados"][0]["series"][0]["serie"].items()):
 macae_pop_fig = px.bar(macae_population_df,x="Ano",y="População",title="População ao Longo do Tempo")
 
 macae_pop_fig.update_xaxes(type='category', tickmode='linear', tickvals=macae_population_df['Ano'].tolist())
+
+macae_pop_fig.update_traces(
+    hovertemplate="<br>".join([
+        "Ano: %{x}",
+        "População: %{y}"
+    ])
+)
 
 #IBGE dados piramide etaria
 data = requests.get("https://servicodados.ibge.gov.br/api/v3/agregados/9606/periodos/2022/variaveis/93?localidades=N6[3302403]&classificacao=2[4,5]|287[93070,93084,93085,93086,93087,93088,93089,93090,93091,93092,93093,93094,93095,93096,93097,93098,49108,49109,60040,60041,6653]")
@@ -76,7 +94,8 @@ trace_male = go.Bar(
     x=df_dist_idade_sexo["Quantidade"][df_dist_idade_sexo["Sexo"] == "Homens"],
     orientation='h',
     name='Homens',
-    hoverinfo='x+text',
+    hoverinfo='text',
+    text=df_dist_idade_sexo["Quantidade"][df_dist_idade_sexo["Sexo"] == "Homens"],
     marker=dict(color='blue')
 )
 
@@ -85,14 +104,20 @@ trace_female = go.Bar(
     x=df_dist_idade_sexo["Quantidade"][df_dist_idade_sexo["Sexo"] == "Mulheres"] * -1,  # Multiply by -1 to mirror the female bars
     orientation='h',
     name='Mulheres',
-    hoverinfo='x',
+    hoverinfo='text',
+    text=df_dist_idade_sexo["Quantidade"][df_dist_idade_sexo["Sexo"] == "Mulheres"],
     marker=dict(color='pink')
 )
 
 layout = go.Layout(
     title='Piramide Etaria',
     barmode='overlay',  # Overlay male and female bars
-    xaxis=dict(title='População', range=[-max(max(df_dist_idade_sexo["Quantidade"][df_dist_idade_sexo["Sexo"] == "Homens"]), max(df_dist_idade_sexo["Quantidade"][df_dist_idade_sexo["Sexo"] == "Mulheres"])) * 1.1, max(max(df_dist_idade_sexo["Quantidade"][df_dist_idade_sexo["Sexo"] == "Homens"]), max(df_dist_idade_sexo["Quantidade"][df_dist_idade_sexo["Sexo"] == "Mulheres"])) * 1.1]),
+    xaxis=dict(
+        title='População', 
+        range=[-15000,15000],
+        tickvals=[-10000, -5000, 0, 5000, 10000],
+        ticktext=["10k", "5k", 0, "5k", "10k"]
+    ),
     yaxis=dict(title='Faixa Etaria'),
 )
 
@@ -122,8 +147,14 @@ sun_pop_fig = px.sunburst(
     path=['Cor', 'Sexo', 'Idade'],
     values='População',
     title='Grafico Solar da População',
-    color_discrete_sequence=["#967f51", "#E6D1B1","#1f1e1c", "#f7cd79", "#454128"],#['rgb(140, 95, 6)','rgb(227, 203, 182)','rgb(51, 37, 10)','rgb(242, 180, 61)','rgb(117, 107, 16)']
+    color_discrete_sequence=["#967f51", "#E6D1B1","#1f1e1c", "#f7cd79", "#454128"],
     width=800,
     height=800
+)
+
+sun_pop_fig.update_traces(
+    hovertemplate="<br>".join([
+        "População: %{value}"
+    ])
 )
 
